f4b0dfe35ca7e7d9ef985945d54d5f9ce5e391c3
AlfonS5S5S5 <r.jonathanalfons@gmail.com>
2025-05-25 10:26:16 +0700

    sync

--- frontend/src/config/Route.tsx
+++ frontend/src/config/Route.tsx
@@ -5,22 +5,15 @@ import CekPinPage from "../page/cekPinPage";
 import CSHomePage from '../page/CSHomePage';
 import CSLogin from '../page/CSLogin';
 import CSReportPage from '../page/CSReportPage';
-<<<<<<< HEAD
-import InfoSaldo from "../page/DetailHomePage/InfoSaldo";
-import { MInfo } from "../page/DetailHomePage/MInfo";
-import { MPayment } from "../page/DetailHomePage/MPayment";
-import { MTransfer } from "../page/DetailHomePage/MTransfer";
-import MutasiRekening from "../page/DetailHomePage/MutasiRekening";
-=======
-import { MInfo } from "../page/DetailHomePage/MInfo";
 import InfoSaldo from "../page/DetailHomePage/DetailMInfo/InfoSaldo";
 import MutasiRekening from "../page/DetailHomePage/DetailMInfo/MutasiRekening";
+import TopUp from "../page/DetailHomePage/DetailMTransfer/TopUp";
+import Transfer from "../page/DetailHomePage/DetailMTransfer/Transfer";
+import { MInfo } from "../page/DetailHomePage/MInfo";
 import { MPayment } from "../page/DetailHomePage/MPayment";
 import MTransfer from "../page/DetailHomePage/MTransfer";
-import  Transfer  from "../page/DetailHomePage/DetailMTransfer/Transfer";
-import  TopUp  from "../page/DetailHomePage/DetailMTransfer/TopUp";
->>>>>>> e1801b071aa608f0177368fb68330cc95d0e4748
 import { Setting } from "../page/DetailHomePage/Setting";
+import EReceipt from "../page/E-Receipt";
 import GantiPassword from '../page/GantiPassword';
 import { GantiPin } from "../page/GantiPin";
 import HomePage from "../page/HomePage";
@@ -28,7 +21,6 @@ import LoginPage from "../page/Login";
 import RegisterPage from "../page/Register";
 import PinPage from "../page/SetPinPage";
 import UserLayout from "../page/UserLayout";
-import EReceipt from "../page/E-Receipt";
 
 const checkAuth = () => localStorage.getItem("token") !== null;
 
@@ -48,16 +40,9 @@ const Routers = createBrowserRouter([
   },
   {
     path: "/user",
-<<<<<<< HEAD
     element: checkAuth() ? <UserLayout /> : <Navigate to="/auth/login" />,    children: [
       { index: true, element: <HomePage /> },
       { path: "set-pin", element: <PinPage /> },
-=======
-    element: checkAuth() ? <UserLayout /> : <Navigate to="/auth/login" />,    
-    children: [
-      { index: true, element: <HomePage /> },      
-      { path: "set-pin", element: <PinPage /> },      
->>>>>>> e1801b071aa608f0177368fb68330cc95d0e4748
       { path: "verify-pin", element: <CekPinPage /> },
       { path: "minfo", element: <MInfo />},
       { path: "mtransfer", element: <MTransfer />},
